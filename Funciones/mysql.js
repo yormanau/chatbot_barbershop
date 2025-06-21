68cde8516fbe5b47aa9b2ec781cd471e83a22772
yormanau <109599510+yormanau@users.noreply.github.com>
2025-06-21 16:39:37 -0500

    Add timezone

--- Funciones/mysql.js
+++ Funciones/mysql.js
@@ -11,7 +11,8 @@ const connection = mysql.createConnection({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT
+  port: process.env.PORT,
+  timezone: '-05:00', 
 });
 
 
