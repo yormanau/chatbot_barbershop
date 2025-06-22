26f1e5d47bd070beb859923c58790d77e0faf8af
yormanau <109599510+yormanau@users.noreply.github.com>
2025-06-22 17:52:00 -0500

    Cron

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const { delay, ALL_WA_PATCH_NAMES } = require('@whiskeysockets/baileys')
 
 
 cron.schedule('* * * * *', async () => {
-  console.log('Enviar notificación activado')
   try {
     const reservas = await enviar_notificacion(); // Obtiene reservas con estado PENDIENTE
 
